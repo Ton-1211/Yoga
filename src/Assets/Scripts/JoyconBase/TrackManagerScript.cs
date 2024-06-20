ef14174fd6b1e4ad2f677b37c6ad0e0e0227a7ec
Ton-1211 <113411399+Ton-1211@users.noreply.github.com>
2024-06-20 19:51:33 +0900

    ローパスフィルターを用いた重力加速度の除去

--- src/Assets/Scripts/JoyconBase/TrackManagerScript.cs
+++ src/Assets/Scripts/JoyconBase/TrackManagerScript.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 class TrackPoint
 {
     [Header("トラッキングで動かすオブジェクト"), SerializeField] Transform TrackTransform;
-    public Joycon TrackJoycon { get; set; }
+    public S_Joycon TrackJoycon { get; set; }
 
     public Transform GetTransform() { return TrackTransform; }
 
@@ -23,18 +23,38 @@ public class TrackManagerScript : MonoBehaviour
 {
     [SerializeField] TrackPoint[] trackPoints;
 
-    private static readonly Joycon.Button[] m_buttons =
-        Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
+    private static readonly S_Joycon.Button[] m_buttons =
+        Enum.GetValues(typeof(S_Joycon.Button)) as S_Joycon.Button[];
 
-    private List<Joycon> m_joycons;
-    private Joycon m_joyconL;
-    private Joycon m_joyconR;
-    private Joycon.Button? m_pressedButtonL;
-    private Joycon.Button? m_pressedButtonR;
+    private List<S_Joycon> m_joycons;
+    private S_Joycon m_joyconL;
+    private S_Joycon m_joyconR;
+    private S_Joycon.Button? m_pressedButtonL;
+    private S_Joycon.Button? m_pressedButtonR;
     private void Start()
     {
         SetControllers();
         SetTrackPoints();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (TrackPoint trackPoint in trackPoints)
+        {
+            Vector3 accel = trackPoint.TrackJoycon.GetAccelWithoutGravity();
+            //accel -= trackPoint.GetTransform().eulerAngles.normalized;
+            Vector3 moveAmount = new Vector3(-accel.y, accel.z, -accel.x) * Time.deltaTime * Time.deltaTime * 9.8f * 300f;
+            //Vector3 moveAmount = new Vector3(-trackPoint.TrackJoycon.GetAccel().y, -trackPoint.TrackJoycon.GetAccel().x, -trackPoint.TrackJoycon.GetAccel().z)
+            //    * Time.deltaTime * Time.deltaTime * 1000f;// ジョイコンの加速度センサはxが縦、yが横方向だったり+-がUnityと逆だったりするのでUnityに揃えた
+            //                                           // 倍率の数字は後で変数にする
+
+            Quaternion orientation = trackPoint.TrackJoycon.GetVector();
+            //orientation = new Quaternion(orientation.x, orientation.z, orientation.y, orientation.w);
+            trackPoint.GetTransform().rotation = orientation;
+            trackPoint.GetTransform().Rotate(90, 0, 0, Space.World);
+
+            trackPoint.AddPosition(moveAmount);
+        }
     }
 
     private void Update()
@@ -49,6 +69,10 @@ public class TrackManagerScript : MonoBehaviour
             if (m_joyconL.GetButton(button))
             {
                 m_pressedButtonL = button;
+                if(m_pressedButtonL == S_Joycon.Button.DPAD_DOWN)
+                {
+                    ResetTrackPosition(m_joyconL);
+                }
             }
             if (m_joyconR.GetButton(button))
             {
@@ -62,19 +86,6 @@ public class TrackManagerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.X))
         {
             m_joyconR.SetRumble(160, 320, 0.6f, 200);
-        }
-
-        foreach (TrackPoint trackPoint in trackPoints)
-        {
-            Vector3 moveAmount = new Vector3(-trackPoint.TrackJoycon.GetAccel().y, -trackPoint.TrackJoycon.GetAccel().x, -trackPoint.TrackJoycon.GetAccel().z)
-                * Time.deltaTime * Time.deltaTime * 1f;// ジョイコンの加速度センサはxが縦、yが横方向だったり+-がUnityと逆だったりするのでUnityに揃えた
-                                                       // 倍率の数字は後で変数にする
-            Quaternion orientation = trackPoint.TrackJoycon.GetVector();
-            //orientation = new Quaternion(orientation.x, orientation.z, orientation.y, orientation.w);
-            trackPoint.GetTransform().rotation = orientation;
-            trackPoint.GetTransform().Rotate(90, 0, 0, Space.World);
-
-            trackPoint.AddPosition(moveAmount);
         }
     }
 
@@ -98,18 +109,21 @@ public class TrackManagerScript : MonoBehaviour
             return;
         }
         GUILayout.BeginHorizontal(GUILayout.Width(960));
-        foreach (var joycon in m_joycons)
+        foreach (var trackPoint in trackPoints)
         {
-            var isLeft = joycon.isLeft;
+            var isLeft = trackPoint.TrackJoycon.isLeft;
             var name = isLeft ? "Joy-Con (L)" : "Joy-Con (R)";
             var key = isLeft ? "Z キー" : "X キー";
             var button = isLeft ? m_pressedButtonL : m_pressedButtonR;
-            var stick = joycon.GetStick();
-            var gyro = joycon.GetGyro();
-            Vector3 gyroRaw = joycon.GetGyroRaw();
-            var accel = joycon.GetAccel();
-            var orientation = joycon.GetVector();
-            Vector3 angle = joycon.GetVector().eulerAngles;
+            var stick = trackPoint.TrackJoycon.GetStick();
+            var gyro = trackPoint.TrackJoycon.GetGyro();
+            Vector3 gyroRaw = trackPoint.TrackJoycon.GetGyroRaw();
+            var accel_NoG = trackPoint.TrackJoycon.GetAccelWithoutGravity();
+            accel_NoG = new Vector3(-accel_NoG.y, -accel_NoG.z, -accel_NoG.x);
+            var accel = trackPoint.TrackJoycon.GetAccel();
+            accel = new Vector3(-accel.y, -accel.z, -accel.x);
+            var orientation = trackPoint.TrackJoycon.GetVector();
+            Vector3 angle = trackPoint.TrackJoycon.GetVector().eulerAngles;
             GUILayout.BeginVertical(GUILayout.Width(480));
             GUILayout.Label(name);
             GUILayout.Label(key + "：振動");
@@ -117,7 +131,9 @@ public class TrackManagerScript : MonoBehaviour
             GUILayout.Label(string.Format("スティック：({0}, {1})", stick[0], stick[1]));
             GUILayout.Label("ジャイロ：" + gyro);
             //GUILayout.Label("生ジャイロ:" + gyroRaw);
+            //GUILayout.Label("加速度：" + accel);
             GUILayout.Label("加速度：" + accel);
+            GUILayout.Label("補正後：" + accel_NoG);
             GUILayout.Label("傾き：" + orientation);
             GUILayout.Label("オイラー角" + angle);
             GUILayout.EndVertical();
@@ -126,7 +142,7 @@ public class TrackManagerScript : MonoBehaviour
     }
     public void SetControllers()
     {
-        m_joycons = JoyconManager.Instance.j;
+        m_joycons = S_JoyconManager.Instance.j;
         if (m_joycons == null || m_joycons.Count <= 0) return;
         m_joyconL = m_joycons.Find(c => c.isLeft);
         m_joyconR = m_joycons.Find(c => !c.isLeft);
@@ -140,6 +156,18 @@ public class TrackManagerScript : MonoBehaviour
         for (int i = 0; i < loopLength; i++)
         {
             trackPoints[i].TrackJoycon = m_joycons[i];
+        }
+    }
+
+    void ResetTrackPosition(S_Joycon joycon)
+    {
+        foreach(TrackPoint trackpoint in trackPoints)
+        {
+            if(trackpoint.TrackJoycon == joycon)
+            {
+                trackpoint.GetTransform().position = Vector3.zero;// 位置のリセット
+                return;
+            }
         }
     }
 }
