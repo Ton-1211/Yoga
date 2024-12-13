34b85d9bf6bb36d5d226b6c19a8c35b695013f08
Ton-1211 <113411399+Ton-1211@users.noreply.github.com>
2024-12-13 11:37:43 +0900

    攻撃の吸収エフェクトを再生するスクリプトを追加

---
+++ src/Assets/Scripts/PlayerParticleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParticleManager : MonoBehaviour
+{
+    [SerializeField] List<ParticleSystem> particles = new List<ParticleSystem>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayParticle(Vector3 attackPosition)
+    {
+        ParticleSystem playParticle = new ParticleSystem();
+        float minDistance = 0f;
+
+        foreach(ParticleSystem particle in particles)
+        {
+            float distance = Vector3.Distance(attackPosition, particle.transform.position);
+            if(minDistance > distance)
+            {
+                minDistance = distance;
+                playParticle = particle;
+            }
+        }
+
+        playParticle.Play();
+    }
+}
